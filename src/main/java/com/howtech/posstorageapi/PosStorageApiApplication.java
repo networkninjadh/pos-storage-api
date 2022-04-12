690bf4b5a71d334b0c27adc8a9b65e21b5484f2a
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:20:06 -0500

    initial commit

---
+++ src/main/java/com/howtech/posstorageapi/PosStorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.posstorageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosStorageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/posstorageapi/PosStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.posstorageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
